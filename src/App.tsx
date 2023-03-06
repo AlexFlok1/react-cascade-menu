9b1c8b47490664ce1862c32e57833014d9a4836d
Alex Flok <alexflok2018@gmail.com>
2023-03-05 20:24:59 -0500

    using json object to create cascade menu; wip: handle identical components in different submenus

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import CascadeMenu from './cascadeMenu';
+import { MenuItemT } from './cascadeMenu/types';
 
-function App() {
+const App: FC = () => {
+  const menu: MenuItemT[] = [
+    {
+      key: '1',
+      itemContent: <>Menu 1</>,
+    },
+    {
+      key: '2',
+      itemContent: <>Menu 2</>,
+    },
+    {
+      key: '3',
+      itemContent: <>Menu 3</>,
+      subMenu: {
+        items: [
+          {
+            key: '4',
+            itemContent: (
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateTimePicker label='Basic date time picker' />
+              </LocalizationProvider>
+            ),
+          },
+          {
+            key: '5',
+            itemContent: <>Sub Item 2</>,
+            subMenu: {
+              items: [
+                {
+                  key: '6',
+                  itemContent: (
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DateTimePicker label='Basic date time picker' />
+                    </LocalizationProvider>
+                  ),
+                },
+              ],
+              spacing: '0px 0px 0px 15px',
+            },
+          },
+        ],
+        spacing: '0px 0px 0px 15px',
+      },
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CascadeMenu
+        triggerElement={
+          <span style={{ width: '50px', padding: '10px' }}>Menu</span>
+        }
+        menu={menu}
+      />
     </div>
   );
-}
+};
 
 export default App;
